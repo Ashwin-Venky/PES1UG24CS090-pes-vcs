15ff6f02f4c7d710c14984d4a284de6b25d35288
Ashwin-Venky <ashwinvenky1312@gmail.com>
2026-04-20 15:50:01 +0530

    Phase4: Add author and timestamp to commit structure

--- commit.c
+++ commit.c
@@ -203,6 +203,10 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         c.has_parent = 1;
     else
         c.has_parent = 0;
+    
+     snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
 
     return 0;
 }
